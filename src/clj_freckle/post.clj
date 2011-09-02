7758c05cb99cff96ba298e08e46cb8aee631ea58
Tom Hickey <thickey@gmail.com>
2011-09-02 17:17:54 -0400

    Only import entries works. all other paths are leading to 302 errors now (even the core/get-xyz requests)

--- src/clj_freckle/post.clj
+++ src/clj_freckle/post.clj
@@ -41,37 +41,6 @@
                   :entry)]
     (vec details)))
 
-;(defn create-entry-xml
-;  "Creates the xml necessary to post a single time entry.
-;Defaulting date to today (formated in YYYY-MM-DD)"
-;  ([minutes user project-id description]
-;    (create-entry-xml minutes user project-id description (util/format-iso-date (Date.))))
-;  ([minutes user project-id description date]
-;    (prxml/prxml
-;      [:decl! {:version "1.0"}] 
-;      [:entry
-;       [:minutes minutes] 
-;       [:user user]
-;       [:project-id {:type "integer"} project-id]
-;       [:description description]
-;       [:date date]])))
-;
-;(defn create-import-entry-xml
-;  "Creates the xml necessary when posting multiple time entries.
-;Defaulting date to today (formated in YYYY-MM-DD)"
-;  ([minutes user project-name description]
-;    (create-entry-xml minutes user project-name description (util/format-iso-date (Date.))))
-;  ([minutes user project-name description date]
-;    (prxml/prxml
-;      [:decl! {:version "1.0"}] 
-;      [:entry
-;       [:minutes minutes] 
-;       [:user user]
-;       [:project-name project-name]
-;       [:description description]
-;       [:date date]])))
-
-
 (defn import-entries
   "POST to /api/entries/import.xml"
   [{:keys [token client] :as creds} entries]
@@ -90,32 +59,32 @@
   (import-entries creds [(apply create-import-entry entry-details)]))
 
 
-(defn post-entry
-  "POST to /api/entries.xml"
-  [{:keys [token client] :as creds} & entry-details]
-  (let [entry (apply create-entry entry-details)
-        xml (with-out-str (prxml/prxml [:decl! {:version "1.0"}]
-                            (prep-xml-entry entry)))
-        url (freckle/xml-resource-url creds "entries")]
-    (client/post url
-      {:body xml
-       :headers {"X-FreckleToken:" token}
-       :content-type "text/xml"})))
+;(defn post-entry
+;  "POST to /api/entries.xml"
+;  [{:keys [token client] :as creds} & entry-details]
+;  (let [entry (apply create-entry entry-details)
+;        xml (with-out-str (prxml/prxml [:decl! {:version "1.0"}]
+;                            (prep-xml-entry entry)))
+;        url (freckle/xml-resource-url creds "entries")]
+;    (client/post url
+;      {:body xml
+;       :headers {"X-FreckleToken:" token}
+;       :content-type "text/xml"})))
 
 
 
-(defn post-json-entry
-  "POST to /api/entries.xml"
-  [{:keys [token client] :as creds} & entry-details]
-  (let [entry (apply create-entry entry-details)
-        url (freckle/json-resource-url creds "entries")]
-    (client/post url
-      {:body 
-;       (client/generate-query-string
-;         (reduce (fn [m [k v]]
-;                   (assoc m (str "entry[" (name k) "]") v))
-;           {} entry))
-       (apply str (interpose "&"
-                    (map (fn [[k v]] (str "entry[" (name k) "]" v)) entry)))
-       :headers {"X-FreckleToken:" token}
-       :content-type :json})))
+;(defn post-json-entry
+;  "POST to /api/entries.xml"
+;  [{:keys [token client] :as creds} & entry-details]
+;  (let [entry (apply create-entry entry-details)
+;        url (freckle/json-resource-url creds "entries")]
+;    (client/post url
+;      {:body
+;;       (client/generate-query-string
+;;         (reduce (fn [m [k v]]
+;;                   (assoc m (str "entry[" (name k) "]") v))
+;;           {} entry))
+;       (apply str (interpose "&"
+;                    (map (fn [[k v]] (str "entry[" (name k) "]" v)) entry)))
+;       :headers {"X-FreckleToken:" token}
+;       :content-type :json})))
